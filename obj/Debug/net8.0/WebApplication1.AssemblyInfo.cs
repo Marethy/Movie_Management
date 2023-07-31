1863a6ee9a6937fa8f0e4fbeaccf6abffaa42cbd
Marethy <khiemmm2004@gmail.com>
2023-08-01 01:54:29 +0700

    commit 0 of 2023-08-01 with count 1

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ab1ace2b43cfff4923ff148f3ad23c0d230c05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2675b826270d14d2d40559b2f03bd7c7b2680ade")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
